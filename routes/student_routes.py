a5a9a5032a692b2898223cb764ff88e706fcc0e9
mahi-codes-dev <maheshlute2005@gmail.com>
2026-03-16 03:22:50 +0530

    Refactor: introduce service layer and move student logic to services module

--- routes/student_routes.py
+++ routes/student_routes.py
@@ -1,31 +1,13 @@
-from flask import Blueprint, jsonify, request
-from database import get_db_connection
-import psycopg2
+from flask import Blueprint, jsonify
+from services.student_service import fetch_all_students
 
 student_bp = Blueprint("student_bp", __name__)
 
 @student_bp.route("/students")
 def get_students():
     try:
-        conn = get_db_connection()
-        cur = conn.cursor()
-
-        cur.execute("SELECT * FROM students;")
-        rows = cur.fetchall()
-
-        students = []
-        for row in rows:
-            students.append({
-                "id": row[0],
-                "name": row[1],
-                "email": row[2],
-                "department": row[3]
-            })
-
-        cur.close()
-        conn.close()
-
-        return jsonify(students)
+        students = fetch_all_students()
+        return jsonify(students), 200
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
